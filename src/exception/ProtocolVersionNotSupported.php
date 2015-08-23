2b8c731484997bde741270f9cff20d618ceb3f72
Vitaly Iegorov <egorov@samsonos.com>
2015-08-23 14:00:43 +0300

    Added class variables Implemented protocol version

---
+++ src/exception/ProtocolVersionNotSupported.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egorov
+ * Date: 23.08.2015
+ * Time: 13:57
+ */
+namespace samsonframework\psr\exception;
+
+/**
+ * HTTP protocol version is not supported
+ * @package samsonframework\psr\exception
+ */
+class ProtocolVersionNotSupported extends \Exception {
+
+}
